69241363ac9a09df2bad6b664d4f3f4eb5571917
GogoTomat <88964256+GogoTomat@users.noreply.github.com>
2021-11-08 14:32:13 +0500

    Update Hw2.sql

--- Hw2.sql
+++ Hw2.sql
@@ -1,16 +1,14 @@
 create table if not exists artist(
     artist_id serial primary key,
     artist_name text,
-    nickname text,
-    album_list text
+    nickname text
 );
 
 create table if not exists album(
     album_id serial primary key,
     artist_id integer references artist(artist_id),
     album_name text,
-    year varchar(4),
-    track_list text
+    year integer
 );
 
 create table if not exists track(
